aba828affc54ed6e014c16111483fc29590dd68f
AbhishekRauthan <abhishek.rauthan.10@gmail.com>
2021-03-08 14:00:58 +0530

    fixed scss support

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,11 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
 
-import * as sassMiddleware from "node-sass-middleware";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.use(sassMiddleware({
-    src: join(__dirname, '..', 'scss'),
-    dest: join(__dirname, '..', 'public'),
-    debug: true,
-    outputStyle: 'compressed'
-  }))
-
-  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.useStaticAssets(join(__dirname, '..', 'views', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
 
